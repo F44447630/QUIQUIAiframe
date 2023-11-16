ca6b1330ef537f123a79598bf3c463979ee37ff8
FDILISIO <134226916+F44447630@users.noreply.github.com>
2023-11-16 20:16:06 -0300

    Add files via upload

---
+++ reservar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Manejo de Reserva</title>
+    <!-- Enlaces a los archivos CSS de Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" 
+</head>
+<body>
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <h1>Manejo de Reserva</h1>
+            
+            <div class="alert alert-success" role="alert">Su reserva fue realizada con exito.</div>      
+
+<!-- Enlaces a los archivos JavaScript de Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
